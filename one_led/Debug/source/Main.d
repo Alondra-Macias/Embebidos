7617a44db27b6f2bd426b345cf1e8f76364fd0b6
Alondra-Macias <ie698198@iteso.mx>
2018-09-01 17:04:25 -0500

    first version of Tarea LED

---
+++ one_led/Debug/source/Main.d
@@ -0,0 +1,18 @@
+source/Main.o source/Main.d: ../source/Main.c \
+ ../source/DataTypeDefinitions.h ../CMSIS/MK64F12.h ../CMSIS/core_cm4.h \
+ ../CMSIS/cmsis_version.h ../CMSIS/cmsis_compiler.h ../CMSIS/cmsis_gcc.h \
+ ../CMSIS/system_MK64F12.h
+
+../source/DataTypeDefinitions.h:
+
+../CMSIS/MK64F12.h:
+
+../CMSIS/core_cm4.h:
+
+../CMSIS/cmsis_version.h:
+
+../CMSIS/cmsis_compiler.h:
+
+../CMSIS/cmsis_gcc.h:
+
+../CMSIS/system_MK64F12.h:
